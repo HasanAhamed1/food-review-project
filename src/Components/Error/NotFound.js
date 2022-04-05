49ff534ccbbc81aef11c762225ee5ce6ebc2fb55
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-04-05 21:44:40 +0600

    error-handle

--- src/Components/Error/NotFound.js
+++ src/Components/Error/NotFound.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="grid justify-items-center">
+      <img
+        src="https://img.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg?w=740&t=st=1649172421~exp=1649173021~hmac=55826cbdf3c2643a5968ce9c4fd23dc784e983b59f34b7528522425d0ad64870"
+        alt=""
+        srcset=""
+      />
+    </div>
+  );
 };
 
 export default NotFound;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import CustomLink from '../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
-        <div className='flex flex-row justify-center gap-10 m-8 font-bold'>
+        <div id='navbar' className='flex flex-row justify-center gap-10 m-8 font-bold'>
             <CustomLink to="/">HOME</CustomLink>
             <CustomLink to="/reviews">REVIEWS</CustomLink>
             <CustomLink to="/dashboard">DASHBOARD</CustomLink>
